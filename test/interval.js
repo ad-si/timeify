a5842e10f93e62ba25737b47e5eab7ad29366c9b
Adrian Sieber <mail@adriansieber.com>
2015-12-20 12:23:26 +0100

    Add test for / as interval-separator

--- test/interval.js
+++ test/interval.js
@@ -2,24 +2,36 @@ import runTest from 'ava'
 import expect from 'unexpected'
 import Hour from '../build/index.js'
 
+function getYearInterval (title) {
+	return {
+		type: 'period',
+		string: title,
+		start: {
+			lowerLimit: new Date('2015-01-01T00:00:00.000Z'),
+			upperLimit: new Date('2016-01-01T00:00:00.000Z'),
+			precision: 'year'
+		},
+		end: {
+			lowerLimit: new Date('2016-01-01T00:00:00.000Z'),
+			upperLimit: new Date('2017-01-01T00:00:00.000Z'),
+			precision: 'year'
+		}
+	}
+}
+
+runTest('2015/2016', test => {
+	expect(
+		new Hour(test.title).toObject(),
+		'to equal',
+		getYearInterval(test.title)
+	)
+})
+
 runTest('2015--2016', test => {
 	expect(
 		new Hour(test.title).toObject(),
 		'to equal',
-		{
-			type: 'period',
-			string: test.title,
-			start: {
-				lowerLimit: new Date('2015-01-01T00:00:00.000Z'),
-				upperLimit: new Date('2016-01-01T00:00:00.000Z'),
-				precision: 'year'
-			},
-			end: {
-				lowerLimit: new Date('2016-01-01T00:00:00.000Z'),
-				upperLimit: new Date('2017-01-01T00:00:00.000Z'),
-				precision: 'year'
-			}
-		}
+		getYearInterval(test.title)
 	)
 })
 
