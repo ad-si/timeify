df9f33dc2517d54b97e3fa3bb761cc49ead6bb67
Adrian Sieber <mail@adriansieber.com>
2016-03-07 14:46:20 +0100

    Test setters & getters of years, months and days

--- test/api.js
+++ test/api.js
@@ -3,6 +3,33 @@ import expect from 'unexpected'
 import Hour from '../build/index.js'
 
 
+runTest('set & get years', test => {
+	const moment = new Hour('2015-11-24T18:00')
+	moment.setYears(1995)
+	expect(moment.years, 'to equal', 1995)
+	moment.years = 1998
+	expect(moment.years, 'to equal', 1998)
+})
+
+
+runTest('set & get months', test => {
+	const moment = new Hour('2015-11-24T18:00')
+	moment.setMonths(9)
+	expect(moment.months, 'to equal', 9)
+	moment.months = 10
+	expect(moment.months, 'to equal', 10)
+})
+
+
+runTest('set & get days', test => {
+	const moment = new Hour('2015-11-24T18:00')
+	moment.setDays(22)
+	expect(moment.days, 'to equal', 22)
+	moment.days = 23
+	expect(moment.days, 'to equal', 23)
+})
+
+
 runTest('set & get hours', test => {
 	const moment = new Hour('2015-11-24T18:00')
 	moment.setHours(12)
